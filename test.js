bf21711eaac88d40a01330c4dfa66368273dd16e
ssc9811 <poer2418@naver.com>
2022-09-25 13:51:21 +0900

    20220925

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-const absolutes = [4, 7, 12]
-const signs = [true, false, true]
-let result = 0
-absolutes.map((absolute, idx) => {
-  result += signs[idx] ? absolute : -absolute
-})
+const a1 = 3
+const a2 = 4
 
-console.log(result)
+
+// const waterMelon = n => {
+//   return '수박'.repeat(n / 2) + (n % 2 === 1 ? '수' : '');
+// }
+
+const waterMelon = n => {
+  console.log('수박'.repeat(n / 2) + (n % 2 === 1 ? '수' : ''));
+}
+
+waterMelon(5)
