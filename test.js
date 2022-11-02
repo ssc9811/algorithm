fab5124f212e50638c42906d396f6ddec27db8d9
ssc9811 <poer2418@naver.com>
2022-11-02 18:58:42 +0900

    20221102

--- test.js
+++ test.js
@@ -1,23 +1,52 @@
-const participant = ["marina", "josipa", "nikola", "vinko", "filipa"];
-const completion = ["josipa", "filipa", "marina", "nikola"];
+const numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2];
+const hand = "left";
+function solution(numbers, hand) {
+  let currentLeft = 10;
+  let currentRight = 12;
 
-function solution(participant, completion) {
-  participant = participant.sort();
-  completion = completion.sort();
-  for (let i = 0; i < participant.length; i++) {
-    if (participant[i] !== completion[i]) {
-    }
-  }
-  return participant;
+  const getDistance = (a, b) => {
+    return (
+      Math.sqrt(Math.pow(a[0] - b[0], 2)) + Math.sqrt(Math.pow(a[1] - b[1], 2))
+    );
+  };
+  const getNumLocation = (num) => {
+    return [Math.floor((num - 1) / 3), Math.floor((num - 1) % 3)];
+  };
+  const toLeft = (num) => {
+    currentLeft = num;
+    return "L";
+  };
+  const toRight = (num) => {
+    currentRight = num;
+    return "R";
+  };
+
+  return numbers
+    .map((number) => {
+      if (number === 0) {
+        number = 11;
+      }
+      if (number % 3 === 1) {
+        return toLeft(number);
+      } else if (number % 3 === 0) {
+        return toRight(number);
+      } else {
+        const currentNumLocation = getNumLocation(number);
+        const leftNumLocation = getNumLocation(currentLeft);
+        const rightNumLocation = getNumLocation(currentRight);
+        const leftDistance = getDistance(leftNumLocation, currentNumLocation);
+        const rightDistance = getDistance(rightNumLocation, currentNumLocation);
+
+        if (leftDistance === rightDistance) {
+          return hand === "right" ? toRight(number) : toLeft(number);
+        } else if (leftDistance < rightDistance) {
+          return toLeft(number);
+        } else {
+          return toRight(number);
+        }
+      }
+    })
+    .join("");
 }
 
-solution(participant, completion);
-
-// console.log(participant.join());
-
-// function solution(participant, completion) {
-// completion.map((p) => {
-//   participant.splice(participant.indexOf(p), 1);
-// });
-// return participant.join();
-// }
+solution(numbers, hand);
