546de625ac9ca4d96fd43516cbd721bceeeba86c
ssc9811 <poer2418@naver.com>
2022-11-05 15:41:14 +0900

    20221105

--- test.js
+++ test.js
@@ -1,35 +1,31 @@
-const n = 5;
-function solution(n) {
-  const fibonacciArr = new Array(n + 1).fill(0);
-  fibonacciArr[0] = 0;
-  fibonacciArr[1] = 1;
-  let answer = 0;
-  for (let i = 2; i < fibonacciArr.length; i++) {
-    answer = fibonacciArr[i] =
-      (fibonacciArr[i - 1] + fibonacciArr[i - 2]) % 1234567;
-  }
+const board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+const moves = [1, 5, 3, 5, 1, 2, 1, 4];
 
+function solution(board, moves) {
+  let answer = 0;
+  let basket = [];
+  moves.forEach((move) => {
+    for (let i = 0; i < board.length; i++) {
+      const item = board[i][move - 1];
+      if (item !== 0) {
+        if (basket[basket.length - 1] !== item) {
+          basket.push(item);
+        } else {
+          basket.pop();
+          answer += 2;
+        }
+        board[i][move - 1] = 0;
+        break; // 0이 아닌 숫자일 경우 한번만 실행되도록
+      }
+    }
+  });
   return answer;
 }
-solution(n);
 
-// function fibonacci1234567(n){ //재귀적으로 하면 시간초과
-//   let fiboArr = new Array(n+1).fill(0)
-//   fiboArr[0] = 0
-//   fiboArr[1] = 1
-//   for(let i=2; i<=n; i++) fiboArr[i] = (fiboArr[i-1] + fiboArr[i-2])%1234567 //1234567로 나눈 나머지로 하지 않으면 정수범위 초과
-//   return fiboArr[n]
-// }
-
-function solution(n) {
-  const fibonacciArr = new Array(n + 1).fill(0);
-  fibonacciArr[0] = 0;
-  fibonacciArr[1] = 1;
-  let answer = 0;
-  for (let i = 2; i < fibonacciArr.length; i++) {
-    answer = fibonacciArr[i] =
-      (fibonacciArr[i - 1] + fibonacciArr[i - 2]) % 1234567;
-  }
-
-  return answer;
-}
+solution(board, moves);
