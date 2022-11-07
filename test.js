638aa87ac613e5bd98ab240f769108ec72247f98
ssc9811 <poer2418@naver.com>
2022-11-07 15:20:24 +0900

    20221107

--- test.js
+++ test.js
@@ -1,23 +1,4 @@
-const n = 78;
+const brown = 10;
+const yellow = 2;
 
-// const a1 = n.toString(2);
-// console.log(parseInt(a1, 2));
-function solution(n) {
-  const oneCount = binaryOne(n);
-  let num = n + 1;
-  while (true) {
-    const nextBinaryOneCount = binaryOne(num);
-    if (nextBinaryOneCount === oneCount) return num;
-    num++;
-  }
-}
-
-function binaryOne(n) {
-  let oneCount = 0;
-  const binary = n.toString(2);
-  for (let i of binary) {
-    if (i === "1") oneCount++;
-  }
-  return oneCount;
-}
-solution(n);
+solution(brown, yellow);
