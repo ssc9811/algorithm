d2561384991cfa63781581c75d5d4081e82312f2
ssc9811 <poer2418@naver.com>
2022-11-14 15:09:19 +0900

    20221114

--- all.md
+++ all.md
@@ -2159,3 +2159,36 @@ function solution(people, limit) {
   return answer;
 }
 ```
+
+<br>
+
+## 2-13 N개의 최소공배수
+
+<br>
+
+![스크린샷 2022-11-14 오후 2 40 51](https://user-images.githubusercontent.com/39263149/201583963-5ef92bd4-d069-4e25-9bbe-fd71ac0c8c40.png)
+
+```javascript
+function solution(arr) {
+  arr.sort((a, b) => a - b);
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+  let answer = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    answer = lcm(answer, arr[i]);
+  }
+  return answer;
+}
+```
+
+### 다른 사람 풀이
+
+```javascript
+function nlcm(num) {
+  return num.reduce((a, b) => (a * b) / gcd(a, b));
+}
+
+function gcd(a, b) {
+  return a % b ? gcd(b, a % b) : b;
+}
+```
--- test.js
+++ test.js
@@ -1,23 +1,22 @@
-const people = [70, 50, 80, 50];
-const limit = 100;
+/*
+유클리드 호제법
 
-function solution(people, limit) {
-  people.sort((a, b) => b - a);
-  let answer = 0;
-  let min = 0;
-  let max = people.length - 1;
-  while (min < max) {
-    let sum = people[min] + people[max];
-    if (sum <= limit) {
-      min++;
-      max--;
-    } else {
-      min++;
-    }
-    answer++;
-  }
-  if (min === max) answer++;
-  console.log(answer);
-}
+< 최대 공약수 >
+if(n < m) 인 경우에, n % m = r 로 표현할 수 있다.
+이때 r === 0 일 경우 m 이 '최대 공약수'가 된다.
 
-solution(people, limit);
+< 최소 공배수 >
+[ n * m / 최대 공약수 ] 를 계산하면 최소 공배수를 구할 수 있다.
+*/
+const arr = [2, 6, 8, 14];
+
+// const gcd = (a, b) => {
+//   while (b > 0) {
+//     let temp = b;
+//     b = a % b;
+//     a = temp;
+//   }
+//   return a;
+// };
+
+solution(arr);
