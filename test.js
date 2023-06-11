dc88dc5e06bd30604ce203ce3208f6574b3ee089
ssc9811 <poer2418@naver.com>
2023-06-11 23:08:44 +0900

    20230611

--- test.js
+++ test.js
@@ -1,16 +1,6 @@
 const input = require("fs")
   .readFileSync("./baekjoon_input.txt")
   .toString()
-  .trim()
-  .split("\n")
-  .map((v) => v.split(" ").map((t) => +t));
+  .trim();
 
-let x = 0;
-let y = 0;
-
-for (let i of input) {
-  const [a, b] = i;
-  x ^= a;
-  y ^= b;
-}
-console.log(x, y);
+console.log(input * 4);
