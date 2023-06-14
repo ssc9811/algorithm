06ab84f982fbe79095fa8839b3e1cd5c98c4a8b4
ssc9811 <poer2418@naver.com>
2023-06-14 09:31:47 +0900

    20230614

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 let n = require("fs").readFileSync("./baekjoon_input.txt").toString().trim();
+let num = BigInt(n);
 
-console.log(`${n * n}\n2`);
+console.log(`${(num * (num - BigInt(1)) * (num - BigInt(2))) / BigInt(6)}`);
+console.log(3);
