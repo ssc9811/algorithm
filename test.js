8bc1a20431b11c2e47faef345749c11496214af2
ssc9811 <poer2418@naver.com>
2023-06-16 09:40:47 +0900

    20230616

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
-let N = require("fs").readFileSync("./baekjoon_input.txt").toString().trim();
+let [a, b, c, d, e, f] = require("fs")
+  .readFileSync("./baekjoon_input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((t) => +t);
 
-let answer = 0;
-let Num = N;
-
-while (Num >= 1) {
-  let divSum = String(Num)
-    .split("")
-    .reduce((acc, cur) => (acc += +cur), 0);
-  let sum = divSum + +Num;
-  if (sum === +N) {
-    answer = Num;
+for (let i = -999; i <= 999; i++) {
+  for (let j = -999; j <= 999; j++) {
+    if (a * i + b * j == c && d * i + e * j == f) {
+      console.log(i, j);
+      break;
+    }
   }
-  Num--;
 }
-
-console.log(answer);
