14d52073ceb58f4f5241dbc68245b3db5e73d535
ssc9811 <poer2418@naver.com>
2023-06-27 20:44:40 +0900

    20230627

--- test.js
+++ test.js
@@ -1,47 +1,17 @@
-const [n, dots] = require("fs")
+const [[n], ...dots] = require("fs")
   .readFileSync("./baekjoon_input.txt")
   .toString()
   .trim()
   .split("\n")
   .map((t) => t.split(" ").map((x) => x));
 
-const set = new Set(dots);
-const uniq = [...set].sort((a, b) => a - b);
+let company = new Map(dots.map((el) => [el[0], el[1]]));
+const result = [];
 
-let dic = {};
-uniq.forEach((e, i) => {
-  dic[e] = i;
-});
-
-let answer = "";
-for (let i = 0; i < dots.length; i++) {
-  answer += dic[dots[i]] + " ";
+for (let key of company.keys()) {
+  if (company.get(key) !== "leave") result.push(key);
 }
 
-console.log(answer);
+result.sort().reverse();
 
-// const sorted = dots.sort((a, b) => a.dot - b.dot);
-
-// console.log(sorted);
-
-// const newObj = sorted.map((item, index) => {
-//   if (index === 0) {
-//     min = item.dot;
-//   }
-//   if (item[1] === min) {
-//     return {item.i, count};
-//   } else {
-//     count++;
-//     min = item.dot;
-//     return [item.i, count];
-//   }
-// });
-
-// console.log("newObj", newObj);
-
-// console.log(
-//   newArray
-//     .sort((a, b) => a[0] - b[0])
-//     .map((item) => item[1])
-//     .join(" ")
-// );
+console.log(result.join(" "));
