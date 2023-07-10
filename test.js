5e084d6c43a9d9c02776b2dc0c033fb87f77c51d
ssc9811 <poer2418@naver.com>
2023-07-10 17:43:35 +0900

    20230710

--- test.js
+++ test.js
@@ -1,40 +1,9 @@
-let [n, ...cmds] = require("fs")
-  .readFileSync("./baekjoon_input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+let S = require("fs").readFileSync("./baekjoon_input.txt").toString().trim();
 
-const deque = [];
-const answer = [];
+const regExp = /<[a-z\s]+>|[a-z0-9]+/g;
 
-for (let cmd of cmds) {
-  let [event, value] = cmd.split(" ");
-  switch (event) {
-    case "push_front":
-      deque.unshift(value);
-      break;
-    case "push_back":
-      deque.push(value);
-      break;
-    case "pop_front":
-      answer.push(deque.length ? deque.shift() : -1);
-      break;
-    case "pop_back":
-      answer.push(deque.length ? deque.pop() : -1);
-      break;
-    case "size":
-      answer.push(deque.length);
-      break;
-    case "empty":
-      answer.push(deque.length ? 0 : 1);
-      break;
-    case "front":
-      answer.push(deque.length ? deque[0] : -1);
-      break;
-    case "back":
-      answer.push(deque.length ? deque[deque.length - 1] : -1);
-      break;
-  }
-}
-
-console.log(answer.join("\n"));
+console.log(
+  S.replace(regExp, (word) =>
+    word.startsWith("<") ? word : word.split("").reverse().join("")
+  )
+);
