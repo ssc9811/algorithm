059ff73d1a4afcf5caaccc2456f32670f67d49f6
ssc9811 <poer2418@naver.com>
2023-07-11 17:34:25 +0900

    20230711

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
-let S = require("fs").readFileSync("./baekjoon_input.txt").toString().trim();
+let input = require("fs")
+  .readFileSync("./baekjoon_input.txt")
+  .toString()
+  .trim()
+  .split("");
 
-const regExp = /<[a-z\s]+>|[a-z0-9]+/g;
+// 스택 활용해서 풀이
+let sticks = [];
+let answer = 0;
 
-console.log(
-  S.replace(regExp, (word) =>
-    word.startsWith("<") ? word : word.split("").reverse().join("")
-  )
-);
+for (let i = 0; i < input.length; i++) {
+  const string = input[i];
+  if (string === "(" && input[i + 1] === ")") {
+    answer += sticks.length;
+    i += 1;
+  } else if (string === "(") {
+    sticks.push("(");
+  } else if (string === ")") {
+    sticks.pop();
+    answer += 1;
+  }
+}
+console.log(answer);
