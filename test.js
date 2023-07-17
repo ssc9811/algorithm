8fd03d3f5723dd3b5d7a84a211785ade489c0c6e
ssc9811 <poer2418@naver.com>
2023-07-17 10:42:45 +0900

    20230717

--- test.js
+++ test.js
@@ -1,17 +1,28 @@
-let [[n], [...nums]] = require("fs")
+let input = require("fs")
   .readFileSync("./test_input.txt")
   .toString()
   .trim()
-  .split("\n")
-  .map((t) => t.split(" ").map((a) => +a));
+  .split("\n")[1]
+  .split(" ")
+  .map((t) => +t);
 
-let tmp = [];
-let result = new Array(nums.length).fill(-1);
+// let tmp = [];
+const count = {};
+let result = new Array(input.length).fill(-1);
 
-for (let i = 0; i < n; i++) {
-  while (tmp.length && nums[tmp[tmp.length - 1]] < nums[i]) {
-    result[tmp.pop()] = nums[i];
+input.forEach((x) => {
+  count[x] = count[x] ? count[x] + 1 : 1;
+});
+
+const stack = [];
+
+for (let i = 0; i < input.length; i++) {
+  while (
+    stack.length &&
+    count[input[stack[stack.length - 1]]] < count[input[i]]
+  ) {
+    result[stack.pop()] = input[i];
   }
-  tmp.push(i);
+  stack.push(i);
 }
 console.log(result.join(" "));
