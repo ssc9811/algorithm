900cc37ecfba893471c90eb0c725a5267a2539e1
ssc9811 <poer2418@naver.com>
2023-07-23 16:06:16 +0900

    20230723

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
-// 소문자, 대문자, 숫자, 공백
-
-const [A, B, C] = require("fs")
+let [O, T] = require("fs")
   .readFileSync("./test_input.txt")
   .toString()
   .trim()
   .split(" ")
   .map((t) => +t);
 
-console.log((A + B) % C);
-console.log(((A % C) + (B % C)) % C);
-console.log((A * B) % C);
-console.log(((A % C) * (B % C)) % C);
+const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+const lcm = (a, b) => (a * b) / gcd(a, b);
+
+console.log(gcd(O, T));
+console.log(lcm(O, T));
