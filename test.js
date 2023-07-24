c602b86b961435956bef48b657c95839b6334333
ssc9811 <poer2418@naver.com>
2023-07-24 17:10:06 +0900

    20230724

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
-let [O, T] = require("fs")
+let [[n], ...nums] = require("fs")
   .readFileSync("./test_input.txt")
   .toString()
   .trim()
-  .split(" ")
-  .map((t) => +t);
+  .split("\n")
+  .map((t) => t.split(" ").map((x) => +x));
 
 const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
 const lcm = (a, b) => (a * b) / gcd(a, b);
 
-console.log(gcd(O, T));
-console.log(lcm(O, T));
+for (num of nums) {
+  const [A, B] = num;
+  console.log(lcm(A, B));
+}
+
+// const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+// const lcm = (a, b) => (a * b) / gcd(a, b);
+
+// console.log(gcd(O, T));
+// console.log(lcm(O, T));
