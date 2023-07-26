1f3b8f6e12bcec64338b5351737ad8bcb7aa3585
ssc9811 <poer2418@naver.com>
2023-07-26 23:20:43 +0900

    20230726

--- test.js
+++ test.js
@@ -1,22 +1,26 @@
-let [[n], [...primes]] = require("fs")
+let [M, N] = require("fs")
   .readFileSync("./test_input.txt")
   .toString()
   .trim()
-  .split("\n")
-  .map((t) => t.split(" ").map((x) => +x));
+  .split(" ")
+  .map((t) => +t);
 
-let answer = 0;
-function isPrime(num) {
-  if (num === 1) {
+const isPrime = (n) => {
+  if (n === 1) {
     return false;
   }
-  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
-    if (num % i === 0) {
+  for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
+    if (n % i === 0) {
       return false;
     }
   }
-  answer += 1;
+  answer += `${n}\n`;
+};
+
+let answer = "";
+
+for (let i = M; i <= N; i++) {
+  isPrime(i);
 }
-for (let prime of primes) {
-  isPrime(prime);
-}
+
+console.log(answer.trim());
