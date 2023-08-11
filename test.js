f2c394fcfaef23f247c0826d3e689e2b3bd44eb7
ssc9811 <poer2418@naver.com>
2023-08-11 17:10:59 +0900

    20230811

--- test.js
+++ test.js
@@ -1,14 +1,5 @@
-let N = +require("fs").readFileSync("./test_input.txt").toString().trim();
+let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+let search = { ...defaults, food: "rich" };
 
-function Factorial(input) {
-  let answer = 0;
-  if (input === 0) answer;
-  for (let i = 1; i <= input; i++) {
-    if (i % 5 === 0) answer++;
-    if (i % 25 === 0) answer++;
-    if (i % 125 === 0) answer++;
-  }
-  return answer;
-}
-
-console.log(Factorial(N));
+console.log(defaults);
+console.log(search);
